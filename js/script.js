e4662ba9691a0374ded8065f43af8834fc1ced1d
AradoChang <89583931+AradoChang@users.noreply.github.com>
2021-12-08 09:31:54 +0900

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-window.load = function(){};
-$(document).ready(function(){
-    // 위로가기 기능
-    $('.gotop').click(function(){
-    	$('html, body').animate({
-    		scrollTop: 0
-    	});
-    });
-});
